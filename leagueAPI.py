16202a82acc5e20626a9183568b0f0fb746497f5
Eric <ericyeonpark@gmail.com>
2021-10-05 22:10:41 -0700

    formatting

--- leagueAPI.py
+++ leagueAPI.py
@@ -8,12 +8,21 @@ api_key = 'RGAPI-cb5df38f-1f55-4f2b-951d-e36e88d6f5d9'
 watcher = LolWatcher(api_key)
 my_region = 'na1'
 
+
+'''
+Access Account Information Data
+'''
 me = watcher.summoner.by_name(my_region, 'Doublelift')
 print(me)
 
+
+'''
+Access Rank Status Data
+'''
 # Return the rank status for Doublelift
 my_ranked_stats = watcher.league.by_summoner(my_region, me['id'])
 print(my_ranked_stats)
+
 
 '''
 Access Match Data
