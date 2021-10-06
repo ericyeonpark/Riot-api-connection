340ffe07d82cb0927125d65b6cbe346c28fc94ba
Eric <ericyeonpark@gmail.com>
2021-10-05 22:38:40 -0700

    add starter code for access data dragon

--- leagueAPI.py
+++ leagueAPI.py
@@ -52,3 +52,26 @@ Access Match Data
 #     participants.append(participants_row)
 # df = pd.DataFrame(participants)
 #print(df)
+
+
+'''
+Access Data Dragon
+'''
+# Check league's latest version
+latest = watcher.data_dragon.versions_for_region(my_region)['n']['champion']
+
+# Get champions static information
+static_champ_list = watcher.data_dragon.champions(latest, False, 'en_US')
+
+# champ static list data to dict for looking up
+champ_dict = {}
+for key in static_champ_list['data']:
+    row = static_champ_list['data'][key]
+    champ_dict[row['key']] = row['id']
+# for row in participants:
+#     print(str(row['champion']) + ' ' + champ_dict[str(row['champion'])])
+#     row['championName'] = champ_dict[str(row['champion'])]
+
+
+
+#print(row) 
